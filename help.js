72ef434d4249685ae701f46fe3acd91c1bfe6072
safebyte <service@safebyte.com>
2015-09-16 03:48:47 +0200

    2.0.2

--- help.js
+++ help.js
@@ -1,26 +1,4 @@
 require('./')();
 
-// log a string
-console.log("They're minerals! Jesus Christ, Marie.");
-// as you know and love it, native methods are fully supported
-
-// start a timer
-console.time('TimerTim');
-
-// build a box
-var box = console.box('I am a child.');
-
-// add a line to our new box
-box.line('I am the 2nd line of the sub box!');
-
-// print the box - always required when using boxes
-box.out();
-
-// print timer
-console.time('TimerTim');
-
-// make noise
-console.beep();
-return;
 // display help
 return console.help();
